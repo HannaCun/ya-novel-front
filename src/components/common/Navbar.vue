adefe0451a611fe3bd2d5d1a276d093c28cc8c3f
Hanns Chung <markshovchancn@gmail.com>
2023-12-08 06:11:33 +0800

    Changed major appearances. Vive la Chen Siyun!

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -1,15 +1,7 @@
- <template>
-  <div class="mainNav" id="mainNav">
+<template>
+  <div class="mainNav" id="mainNav" style="background-color: #000; text-align: center;">
     <div class="box_center cf">
-      <ul class="nav" id="navModule">
-        <li><router-link :to="{ name: 'home' }">首页</router-link></li>
-        <li>
-          <router-link :to="{ name: 'bookclass' }"> 全部作品 </router-link>
-        </li>
-        <li><router-link :to="{ name: 'bookRank' }">排行榜</router-link></li>
-        <!--<li class=""><a href="/pay/index.html">充值</a></li>-->
-        <li><a @click="goAuthor" href="javascript:void(0)">作家专区</a></li>
-      </ul>
+      <h2 style="color: #fff; font-size: 24px;">欢迎你来到一点一横长。在这里，文学是一种生存方式</h2>
     </div>
   </div>
 </template>
@@ -17,37 +9,18 @@
 <script>
 import { reactive, toRefs, onMounted } from "vue";
 import { useRouter, useRoute } from "vue-router";
-import { getToken} from "@/utils/auth";
-import {getAuthorStatus} from "@/api/author"
+import { getToken } from "@/utils/auth";
+import { getAuthorStatus } from "@/api/author";
+
 export default {
   name: "Navbar",
   setup() {
     const route = useRoute();
     const router = useRouter();
-    const goAuthor = async () => {
-      if (!getToken()) {
-        router.push({
-          name: "login",
-        });
-        return;
-      }
 
-      const {data} = await getAuthorStatus();
-      if(data === null){
-          router.push({
-          name: "authorRegister",
-        });
-        return;
-      }
+    // Remove other logic if not needed
 
-      let routeUrl = router.resolve({
-        name: "authorBookList",
-      });
-      window.open(routeUrl.href, "_blank");
-    };
-    return {
-      goAuthor,
-    };
+    return {};
   },
 };
 </script>
--- src/components/common/Top.vue
+++ src/components/common/Top.vue
@@ -1,10 +1,15 @@
- <template>
+<template>
   <div class="topMain">
     <div class="box_center cf">
-      <router-link :to="{ name: 'home' }" class="logo fl"
-        ><img :src="logo" alt="小说精品屋"
-      /></router-link>
-      <div class="searchBar fl">
+      <i class="line mx10" style="background-color: #000; height: 20px;"></i>
+
+      <router-link :to="{ name: 'home' }" class="logo fl" style="text-align: center;">
+        <img :src="logo" alt="一点一横长 一笔到天涯" />
+      </router-link>
+
+      <i class="line mx10" style="background-color: #000; height: 20px;"></i>
+
+      <div class="searchBar fl" style="text-align: center; background-color: rgba(0, 0, 0, 0.5);">
         <div class="search cf">
           <input
             v-model="keyword"
@@ -13,28 +18,26 @@
             class="s_int"
             v-on:keyup.enter="searchByK"
           />
-          <label class="search_btn" id="btnSearch" @click="searchByK()"
-            ><i class="icon"></i
-          ></label>
+          <label class="search_btn" id="btnSearch" @click="searchByK()">
+            <i class="icon"></i>
+          </label>
         </div>
       </div>
 
-      <div class="bookShelf fr" id="headerUserInfo">
-        <!--
-        <a class="sj_link" href="/user/favorites.html">我的书架</a>-->
-        <span v-if="!token" class="user_link"
-          ><!--<i class="line mr20">|</i
-          >-->
+      <i class="line mx10" style="background-color: #000; height: 20px;"></i>
+
+      <div class="bookShelf fr" id="headerUserInfo" style="color: #fff;">
+        <span v-if="!token" class="user_link" style="color: #fff;">
           <router-link :to="{ name: 'login' }" class="mr15">登录</router-link>
-          <router-link :to="{ name: 'register' }" class="mr15"
-            >注册</router-link
-          >
+          <i class="line mx10" style="background-color: #000; height: 20px;"></i>
+          <router-link :to="{ name: 'register' }" class="mr15">注册</router-link>
         </span>
-        <span v-if="token" class="user_link"
-          ><!--<i class="line mr20">|</i
-          >--><router-link :to="{name:'userSetup'}" class="mr15">{{ nickName }}</router-link>
-          <a @click="logout" href="javascript:void(0)">退出</a></span
-        >
+
+        <span v-if="token" class="user_link" style="color: #fff;">
+          <router-link :to="{ name: 'userSetup' }" class="mr15">{{ nickName }}</router-link>
+          <i class="line mx10" style="background-color: #000; height: 20px;"></i>
+          <a @click="logout" href="javascript:void(0)" style="color: #fff;">退出</a>
+        </span>
       </div>
     </div>
   </div>
@@ -44,7 +47,7 @@
 import logo from "@/assets/images/logo.png";
 import { reactive, toRefs, onMounted } from "vue";
 import { useRouter, useRoute } from "vue-router";
-import { getToken, getNickName, removeToken, removeNickName,removeUid } from "@/utils/auth";
+import { getToken, getNickName, removeToken, removeNickName, removeUid } from "@/utils/auth";
 export default {
   name: "Top",
   setup(props, context) {
@@ -65,7 +68,7 @@ export default {
     const logout = () => {
       removeToken();
       removeNickName();
-      removeUid()
+      removeUid();
       state.nickName = "";
       state.token = "";
     };
